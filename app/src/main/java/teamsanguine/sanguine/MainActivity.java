15cd08e342efdebd733ec6145e8a329076cf1d68
Timmy Chandy <timmychandy@hotmail.com>
2015-10-02 17:39:57 -0400

    Added Module to Get GPS Location

--- app/src/main/java/teamsanguine/sanguine/MainActivity.java
+++ app/src/main/java/teamsanguine/sanguine/MainActivity.java
@@ -1,29 +1,57 @@
 package teamsanguine.sanguine;
 
+import android.app.Activity;
+import android.media.browse.MediaBrowser;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    GPSManager gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
+
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                gps = new GPSManager(MainActivity.this);
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                   Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+
+                    Toast.makeText(getApplicationContext(), "Cant Access GPS" , Toast.LENGTH_LONG).show();
+                    gps.showSettingsAlert();
+                }
+
             }
         });
+
+        //gps.stopUsingGPS();
     }
 
+
+
+
+
 }
+
