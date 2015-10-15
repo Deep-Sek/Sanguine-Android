1800877974d8a358f257f9e4486c0768937caae4
Deepan Sekar <deepan.sek@gmail.com>
2015-10-15 17:45:38 -0400

    login screen edit register linked to login + incomplete save new user data to server

---
+++ app/src/main/java/teamsanguine/sanguine/Register.java
@@ -0,0 +1,61 @@
+package teamsanguine.sanguine;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    Button signup;
+    EditText etrepeatpass, etpassword, etusername, etphone, etemail, etname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etname = (EditText) findViewById(R.id.etname);
+        etemail = (EditText) findViewById(R.id.etemail);
+        etphone = (EditText) findViewById(R.id.etphone);
+        etusername = (EditText) findViewById(R.id.etusername);
+        etname = (EditText) findViewById(R.id.etname);
+        etpassword = (EditText) findViewById(R.id.etpassword);
+        etrepeatpass = (EditText) findViewById(R.id.etrepeatpass);
+        signup = (Button) findViewById(R.id.signup);
+
+        signup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signup:
+
+                String name = etname.getText().toString();
+                String username = etusername.getText().toString();
+                String email = etemail.getText().toString();
+                String password = etpassword.getText().toString();
+                int phone = Integer.parseInt(etphone.getText().toString());
+
+                User user = new User(name, username, password, email, phone);
+
+                registerUser(user);
+                break;
+        }
+    }
+
+    private void registerUser(User user){
+        ServerRequests serverRequests = new ServerRequests(this);
+        serverRequests.storeUserDataInBackground(user, new GetUserCallback() {
+            @Override
+            public void done(User returnedUser) {
+                startActivity(new Intent(Register.this, MainActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/teamsanguine/sanguine/loginscreen.java
+++ app/src/main/java/teamsanguine/sanguine/loginscreen.java
@@ -2,21 +2,34 @@ package teamsanguine.sanguine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class loginscreen extends Activity {
-        protected void onCreate(Bundle savedInstanceState) {
+public class loginscreen extends AppCompatActivity implements View.OnClickListener {
+
+    Button hello;
+
+    protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.loginscreen);
+
+            hello = (Button) findViewById(R.id.userCreate);
+            hello.setOnClickListener(this);
 
             //Toast.makeText(getApplicationContext(), "Login Screen", Toast.LENGTH_LONG).show();
         }
 
-        public void onLogIn(View v) {
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, Register.class));
+    }
+
+ /*   public void onLogIn(View v) {
             if (v.getId() == R.id.homeScreen) {
                 EditText a = (EditText) findViewById(R.id.TFusername);
                 String str = a.getText().toString();
@@ -24,5 +37,5 @@ public class loginscreen extends Activity {
                 i.putExtra("Username", str);
                 startActivity(i);
             }
-        }
+        }*/
         }
