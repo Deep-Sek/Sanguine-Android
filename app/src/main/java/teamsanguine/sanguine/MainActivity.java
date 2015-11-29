7723a5afa9293b6d3903e4a91cbb6e3c325058c8
Timmy Chandy <timmychandy@hotmail.com>
2015-11-28 20:08:05 -0500

    Added token registration

--- app/src/main/java/teamsanguine/sanguine/MainActivity.java
+++ app/src/main/java/teamsanguine/sanguine/MainActivity.java
@@ -3,7 +3,9 @@ package teamsanguine.sanguine;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.browse.MediaBrowser;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,13 +14,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends Activity {
     GPSManager gps;
     UserLocalStore userLocalStore;
-
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,5 +98,109 @@ public class MainActivity extends Activity {
     private boolean authenticate(){
         userLocalStore = new UserLocalStore(this);
         return userLocalStore.getUserLoggedIn();
+    }
+
+    private void getRegId() {
+        GoogleCloudMessaging gcmObj;
+        // Check if Google Play Service is installed in Device
+        // Play services is needed to handle GCM stuffs
+        if (checkPlayServices()) {
+            // Register Device in GCM Server
+            getTokenInBackground();
+        }
+    }
+
+    /**
+     * Method to verify google play services on the device
+     * */
+    public boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+    //Get an authorization Token for GCM
+    private void getTokenInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String token;
+                try {
+                    //TODO: get Project number from global variable
+                    InstanceID instanceID = InstanceID.getInstance(getApplicationContext());
+                    token = instanceID.getToken("845607826709", GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+                } catch (final IOException e) {
+                    token = "Error :" + e.getMessage();
+                }
+                return token;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                // Add token to App Server
+                addToken(msg);
+                // Toast.makeText(applicationContext,"Registered with GCM Server successfully.nn"+ msg, Toast.LENGTH_SHORT).show();
+            }
+        }.execute();
+    }
+
+    //This will add the token to App server in the background
+    private void addToken(final String token) {
+        new AsyncTask<Void, Void, String>() {
+
+            private Exception exception;
+            protected String doInBackground(Void... urls) {
+                try {
+                    StringBuilder urlString = new StringBuilder();
+                    String android_id = Settings.Secure.getString(getApplicationContext().getContentResolver(),Settings.Secure.ANDROID_ID);
+                    //TODO: Store the Server IP in global locaiton
+                    urlString.append("http://54.210.175.98:9080//api/GCM/addGCMRegkeySanguine?");
+                    urlString.append("RegKey=").append(token);
+                    urlString.append("&DeviceId=").append( android_id );
+
+                    URL url = new URL(urlString.toString());
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    try {
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                        StringBuilder stringBuilder = new StringBuilder();
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(line).append("\n");
+                        }
+                        bufferedReader.close();
+                        return stringBuilder.toString();
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                } catch (Exception e) {
+                    Log.e("ERROR", e.getMessage(), e);
+                    return null;
+                }
+            }
+
+            protected void onPostExecute(String response) {
+                if (response == null) {
+                    response = "THERE WAS AN ERROR";
+                }
+                else
+                {
+                    //Toast.makeText(applicationContext,"Regn Token Updated to database", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }.execute();
     }
 }
